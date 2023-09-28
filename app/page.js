6d93cacc05a012145302841e2acaed7449690984
ronnie nillo <nilloronniecolayo@gmail.com>
2023-09-28 21:26:52 +0800

    x

--- app/page.js
+++ app/page.js
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-3xl md:text-5xl mt-8 mb-5 text-[#ff6873]">React/Next.js Components</h1>
-      <Link href="/accordion" className="text-lg md:text-xl block max-w-content px-3 py-1">Accordion</Link>
-      <Link href="/tab" className="text-lg md:text-xl blockmax-w-content px-3 py-1">Tab</Link>
+      <Link href="/accordion" className="text-lg md:text-xl block max-w-content hover:text-[#ff6873] py-2">Accordion</Link>
+      <Link href="/tab" className="text-lg md:text-xl blockmax-w-content hover:text-[#ff6873] py-2">Tab</Link>
     </main>
   )
 }
--- app/tab/page.jsx
+++ app/tab/page.jsx
@@ -41,83 +41,77 @@ export default function TabPage() {
 
   const codeSnipet = {
     key: `
-    "use client"
-    
-    import React, { useState } from 'react';
-    import Nav from '../(components)/Nav';
-    import { TbBrandNextjs, TbBrandNuxt, TbBrandSvelte } from "react-icons/tb";
-    
-    export default function TabPage() {
-      const [openTab, setOpenTab] = useState(1);
-    
-      const tabData = [
-        {
-          id: 1,
-          icon: <TbBrandNextjs />,
-          name: 'next.js',
-          body: 'next.js', body: 'The React Framework for the Web Used by some of the worlds largest companies, Next.js enables you to create full-stack Web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.,',
-        },
-        {
-          id: 2,
-          icon: <TbBrandNuxt />,
-          name: 'nuxt.js',
-          body: 'nuxt.js', body: 'The Intuitive Web Framework Build your next Vue.js application with confidence using Nuxt. An open source framework under MIT license that makes web development simple and powerful.,',
-        },
-        {
-          id: 3,
-          icon: <TbBrandSvelte />,
-          name: 'SvelteKit',
-          body: 'Built on Svelte, a UI framework that uses a compiler to let you write breathtakingly concise components that do minimal work in the browser, using languages you already know — HTML, CSS and JavaScript. Its a love letter to web development. But dont take our word for it. Developers consistently rank Svelte as the framework they are most excited about using.,',
-        }
-      ];
-    
-      const handleTabClick = (tabId) => {
-        if (openTab === tabId) {
-          setOpenTab(null);
-        } else {
-          setOpenTab(tabId);
-        }
-      };
 
-    
-      return (
-        <div>
 
-          <h1 className="text-4xl mb-8 mt-8">Tab</h1>
+"use client"
 
-          <div className="border border-slate-100 relative rounded overflow-hidden">
-            
-            <ul className="flex">
-              {tabData.map((item) => (
-                <li key={item.id} className="w-full" id={item.id}>
-                    <a
-                    className={\`text-2xl px-8 py-2 cursor-pointer flex items-center gap-1 text-center $\{
-                    openTab === item.id ? 'bg-[#ff6873] text-white pointer-events-none' : 'bg-[#fff] text-[#ff6873]'
-                    }\`}
-                    onClick={() => handleTabClick(item.id)}
-                >
-                    {item.icon}
-                    {item.name}
-                </a>
-                </li>
-              ))}
-            </ul>
+import React, { useState } from 'react';
+import Nav from '../(components)/Nav';
+import { TbBrandNextjs, TbBrandNuxt, TbBrandSvelte } from "react-icons/tb";
 
-            <div className="py-4 px-4 text-[#30455ccc]">
-              {tabData.map((item) => (
-                <div
-                  key={item.id}
-                  className={\`relative overflow-hidden transition-all ease-in-out delay-150 duration-500 $\{openTab === item.id ? 'max-h-[800px]' : 'max-h-0'}\`}
-                >
-                  <p className={\`py-4 transition-all ease-in-out delay-200 duration-500 $\{openTab === item.id ? 'opacity-1 translate-y-0' : 'opacity-0 translate-y-8'}\`}>{item.body}</p>
-                </div>
-              ))}
-            </div>
+export default function TabPage() {
+  const [openTab, setOpenTab] = useState(1);
 
-          </div>
-        </div>
-      );
+  const tabData = [
+    {
+      id: 1,
+      icon: <TbBrandNextjs />,
+      name: 'next.js',
+      body: 'next.js', body: 'The React Framework for the Web Used by some of the worlds largest companies, Next.js enables you to create full-stack Web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.,',
+    },
+    {
+      id: 2,
+      icon: <TbBrandNuxt />,
+      name: 'nuxt.js',
+      body: 'nuxt.js', body: 'The Intuitive Web Framework Build your next Vue.js application with confidence using Nuxt. An open source framework under MIT license that makes web development simple and powerful.,',
+    },
+    {
+      id: 3,
+      icon: <TbBrandSvelte />,
+      name: 'SvelteKit',
+      body: 'Built on Svelte, a UI framework that uses a compiler to let you write breathtakingly concise components that do minimal work in the browser, using languages you already know — HTML, CSS and JavaScript. Its a love letter to web development. But dont take our word for it. Developers consistently rank Svelte as the framework they are most excited about using.,',
     }
+  ];
+
+  const handleTabClick = (tabId) => {
+    if (openTab === tabId) {
+      setOpenTab(null);
+    } else {
+      setOpenTab(tabId);
+    }
+  };
+
+
+  return (
+    <div>
+
+      <h1 className="text-4xl mb-8 mt-8">Tab</h1>
+
+      <div className="border border-slate-100 relative rounded overflow-hidden">
+        
+        <ul className="flex">
+          {tabData.map((item) => (
+            <li key={item.id} className="w-full" id={item.id}>
+              <a className={\`text-2xl px-8 py-2 cursor-pointer flex items-center gap-1 text-center justify-center $\{openTab === item.id ? 'bg-[#ff6873] text-white pointer-events-none' : 'bg-[#fff] text-[#ff6873]'}\`} onClick={() => handleTabClick(item.id)}>
+                {item.icon}
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="py-4 px-4 text-[#30455ccc]">
+          {tabData.map((item) => (
+            <div key={item.id} className={\`relative overflow-hidden transition-all ease-in-out delay-150 duration-500 $\{openTab === item.id ? 'max-h-[800px]' : 'max-h-0'}\`}>
+              <p className={\`py-4 transition-all ease-in-out delay-200 duration-500 $\{openTab === item.id ? 'opacity-1 translate-y-0' : 'opacity-0 translate-y-8'}\`}>{item.body}</p>
+            </div>
+          ))}
+        </div>
+
+      </div>
+    </div>
+  );
+}
     `
   }
 
@@ -139,11 +133,11 @@ export default function TabPage() {
       <h1 className="text-4xl mb-8 mt-8">Tab</h1>
 
       <div className="border border-slate-100 relative rounded overflow-hidden">
-        <ul className="md:flex">
+        <ul className="sm:flex">
           {tabData.map((item) => (
             <li key={item.id} className="w-full" id={item.id}>
               <a
-                className={`text-2xl px-8 py-2 cursor-pointer flex items-center gap-1 text-center ${
+                className={`text-2xl px-8 py-2 cursor-pointer flex items-center gap-1 text-center justify-center ${
                   openTab === item.id ? 'bg-[#ff6873] text-white pointer-events-none' : 'bg-[#fff] text-[#ff6873]'
                 }`}
                 onClick={() => handleTabClick(item.id)}
